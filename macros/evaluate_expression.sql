c8c236cbaa18344e450a623bc8a5207f51a2c359
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-02-28 16:12:52 +0530

    multicolumnRename - pbt changes

--- macros/evaluate_expression.sql
+++ macros/evaluate_expression.sql
@@ -2,17 +2,15 @@
 
 {% set sql_query = 'select ' ~ expression ~ ' as result' %}
 {% set result = run_query(sql_query) %}
-
  
 {% if result %}
-  {% for row in result %}
-    {{row['result']}}
-  {% endfor %}
-   
+  {%- if execute -%}
+    {% for row in result.rows %}
+      {{row[0]}}
+    {% endfor %}
+  {%- endif -%}
 {% else %}
   {{ log("Query failed or returned no results", info = True) }}
   {{ log(expression, info = True) }}
 {% endif %}
 {% endmacro %}
-
- 

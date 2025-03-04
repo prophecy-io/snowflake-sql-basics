0bdffb30855fe9294534afe0fc6be1cd1c492681
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-03-04 17:07:13 +0530

    adding multi-column rename changes - v1

--- gems/BulkColumnRename.py
+++ gems/BulkColumnRename.py
@@ -1,4 +1,3 @@
-
 from dataclasses import dataclass
 import dataclasses
 
@@ -12,6 +11,8 @@ from pyspark.sql.functions import *
 
 from prophecy.cb.server.base import WorkflowContext
 from prophecy.cb.server.base.datatypes import SInt, SString
+from prophecy.cb.server.base.ComponentBuilderBase import ComponentCode, Diagnostic, SeverityLevelEnum, \
+    SubstituteDisabled, PostSubstituteDisabled
 from prophecy.cb.ui.uispec import *
 import json
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
@@ -29,20 +30,102 @@ class BulkColumnRename(MacroSpec):
         schema: Optional[StructType] = StructType([])
         columnNames: List[str] = field(default_factory=list)
         renameMethod: str = ""
-        prefix: str = ""
+        editOperation: str = ""
+        editType: str = ""
+        editWith: str = ""
         suffix: str = ""
         customExpression: str = ""
         relation: str = ""
 
     def dialog(self) -> Dialog:
+        horizontalDivider = HorizontalDivider()
         relationTextBox = TextBox("Table name").bindPlaceholder("in0").bindProperty("relation")
-        renameMethod = SelectBox("Select a method to rename columns").addOption("Add Prefix", "Add Prefix").addOption("Add Suffix", "Add Suffix").addOption("Custom Expression", "Custom Expression").bindProperty("renameMethod")
-        dialog = Dialog("BulkColumnRename").addElement(ColumnsLayout(gap="1rem", height="100%").addColumn(Ports(allowInputAddOrDelete=True),"content").addColumn(StackLayout(height="100%").addElement(relationTextBox).addElement(renameMethod).addElement(SchemaColumnsDropdown("Select columns to rename").withMultipleSelection().bindSchema("schema").bindProperty("columnNames")).addElement(Condition().ifEqual(PropExpr("component.properties.renameMethod"), StringExpr("Add Prefix")).then(StackLayout().addElement(TextBox("Enter Prefix").bindPlaceholder("""Example: new_""").bindProperty("prefix")))).addElement(Condition().ifEqual(PropExpr("component.properties.renameMethod"), StringExpr("Add Suffix")).then(StackLayout().addElement(TextBox("Enter Suffix").bindPlaceholder("""Example: _new""").bindProperty("suffix")))).addElement(Condition().ifEqual(PropExpr("component.properties.renameMethod"), StringExpr("Custom Expression")).then(StackLayout().addElement(TextBox("Enter Custom Expression").bindPlaceholder("""Example: concat(column_name, 'xyz')""").bindProperty("customExpression"))))))
+        renameMethod = SelectBox("")\
+                        .addOption("Edit prefix/suffix", "editPrefixSuffix")\
+                        .addOption("Advanced rename", "advancedRename")\
+                        .bindProperty("renameMethod")
+        
+        dialog = Dialog("BulkColumnRename")\
+            .addElement(
+                ColumnsLayout(gap="1rem", height="100%")
+                .addColumn(Ports(allowInputAddOrDelete=True),"content")
+                .addColumn(
+                    StackLayout(height="100%")
+                    .addElement(horizontalDivider)
+                    .addElement(relationTextBox)
+                    .addElement(horizontalDivider)
+                    .addElement(TitleElement("Select columns to rename"))
+                    .addElement(
+                        SchemaColumnsDropdown("")
+                        .withMultipleSelection()
+                        .bindSchema("schema")
+                        .bindProperty("columnNames")
+                    )
+                    .addElement(horizontalDivider)
+                    .addElement(TitleElement("Rename method"))
+                    .addElement(renameMethod)
+                    .addElement(
+                        Condition()
+                        .ifEqual(
+                            PropExpr("component.properties.renameMethod"), 
+                            StringExpr("editPrefixSuffix")
+                        )
+                        .then(
+                            ColumnsLayout(gap="1rem", height="100%")
+                            .addColumn(
+                                ColumnsLayout(gap="1rem", height="100%")
+                                .addElement(
+                                    SelectBox("").addOption("Add", "Add").bindProperty("editOperation")
+                                )
+                                .addElement(
+                                    SelectBox("").addOption("Prefix", "Prefix").addOption("Suffix", "Suffix").bindProperty("editType")
+                                )
+                                .addElement(
+                                    TextBox("").bindPlaceholder("new_").bindProperty("editWith")
+                                )                                                                
+                            )
+                        )
+                    )
+                    .addElement(
+                        Condition()
+                        .ifEqual(
+                            PropExpr("component.properties.renameMethod"), 
+                            StringExpr("advancedRename")
+                        )
+                        .then(
+                            StackLayout()
+                            .addElement(
+                                TextBox("Enter Custom Expression")
+                                .bindPlaceholder("""Example: concat(column_name, 'xyz')""")
+                                .bindProperty("customExpression")
+                                )
+                            )
+                    )
+                )
+            )
         return dialog
 
     def validate(self, context: SqlContext, component: Component) -> List[Diagnostic]:
         # Validate the component's state
-        return super().validate(context,component)
+        diagnostics = super(BulkColumnRename, self).validate(context,component)
+        props = component.properties
+
+        if len(component.properties.columnNames) == 0:            
+            diagnostics.append(
+                Diagnostic("component.properties.columnNames", "Select Column to Rename", SeverityLevelEnum.Error))            
+
+        if (
+                component.properties.renameMethod == "editPrefixSuffix" and 
+                (
+                    len(component.properties.editOperation) == 0 or 
+                    len(component.properties.editType) == 0 or  
+                    len(component.properties.editWith) == 0
+                )
+            ):
+            diagnostics.append(
+                Diagnostic("component.properties.renameMethod", "Missing Properties for Edit prefix/suffix Operation", SeverityLevelEnum.Error))
+
+        return diagnostics
 
     def onChange(self, context: SqlContext, oldState: Component, newState: Component) -> Component:
         # Handle changes in the component's state and return the new state
@@ -57,29 +140,36 @@ class BulkColumnRename(MacroSpec):
         return newState.bindProperties(newProperties)
 
     def apply(self, props: BulkColumnRenameProperties) -> str:
+        # Get existing column names
+        column_names = [field.name for field in props.schema.fields]
+
         # generate the actual macro call given the component's state
         resolved_macro_name = f"{self.projectName}.{self.name}"
         arguments = [
             "'" + props.relation + "'",
+            str(column_names),
             str(props.columnNames),
             "'" + str(props.renameMethod) + "'",
-            "'" + str(props.prefix) + "'",
-            "'" + str(props.suffix) + "'",
+            "'" + str(props.editOperation) + "'",
+            "'" + str(props.editType) + "'",
+            "'" + str(props.editWith) + "'",
             '"' + str(props.customExpression) + '"'
         ]
         non_empty_param = ",".join([param for param in arguments if param != ''])
         return f'{{{{ {resolved_macro_name}({non_empty_param}) }}}}'
 
     def loadProperties(self, properties: MacroProperties) -> PropertiesType:
+
         # load the component's state given default macro property representation
         parametersMap = self.convertToParameterMap(properties.parameters)
         return BulkColumnRename.BulkColumnRenameProperties(
-            relation=parametersMap.get('relation')[1:-1],
+            relation=parametersMap.get('relation'),
             columnNames=json.loads(parametersMap.get('columnNames').replace("'", '"')),
-            renameMethod=parametersMap.get('renameMethod')[1:-1],
-            prefix=parametersMap.get('prefix')[1:-1],
-            suffix=parametersMap.get('suffix')[1:-1],
-            customExpression=parametersMap.get('customExpression')[1:-1]
+            renameMethod=parametersMap.get('renameMethod'),
+            editOperation=parametersMap.get('editOperation'),
+            editType=parametersMap.get('editType'),
+            editWith=parametersMap.get('editWith'),
+            customExpression=parametersMap.get('customExpression')
         )
 
     def unloadProperties(self, properties: PropertiesType) -> MacroProperties:
@@ -91,10 +181,9 @@ class BulkColumnRename(MacroSpec):
                 MacroParameter("relation", properties.relation),
                 MacroParameter("columnNames", json.dumps(properties.columnNames)),
                 MacroParameter("renameMethod", properties.renameMethod),
-                MacroParameter("prefix", properties.prefix),
-                MacroParameter("suffix", properties.suffix),
+                MacroParameter("editOperation", properties.editOperation),
+                MacroParameter("editType", properties.editType),
+                MacroParameter("editWith", properties.editWith),
                 MacroParameter("customExpression", properties.customExpression)
             ],
         )
-
-
--- macros/BulkColumnRename.sql
+++ macros/BulkColumnRename.sql
@@ -1,25 +1,65 @@
-{% macro BulkColumnRename(relation, columnNames, renameMethod, prefix='', suffix='', customExpression='') %}
-    {% set renamed_columns = [] %}
+{% macro BulkColumnRename(
+    relation,
+    schema,
+    columnNames,
+    renameMethod,
+    editOperation = '',
+    editType = '',
+    editWith = '',
+    customExpression='') 
+%}
+    {%- set renamed_columns = [] -%}
+    {%- for column in columnNames -%}
+        {%- set renamed_column = "" -%}
+        {%- if renameMethod == 'editPrefixSuffix' -%}
+            {%- if editOperation == 'Add' -%}
+                {%- if editType == 'Prefix' -%}
+                    {%- set renamed_column = "\"" ~  column ~ "\"" ~ " AS \"" ~ editWith ~ column ~ "\"" -%}
+                {%- else -%}
+                    {%- set renamed_column = "\"" ~  column ~ "\"" ~ " AS \"" ~ column ~ editWith ~ "\"" -%}
+                {%- endif -%}
+            {%- endif -%}
 
-    {% for column in columnNames %}
-        {% if renameMethod == 'Add Prefix' %}
-            {% set renamed_column = "\"" ~  column ~ "\"" ~ " AS \"" ~ prefix ~ column ~ "\"" %}
-
-        {% elif renameMethod == 'Add Suffix' %}
-            {% set renamed_column = "\"" ~  column ~ "\"" ~ " AS \"" ~ column ~ suffix ~ "\"" %}
-
-        {% elif renameMethod == 'Custom Expression' %}
-            {% set custom_expr_result = SnowflakeSqlBasics.evaluate_expression(customExpression | replace('column_name',  "\"" ~ column ~ "\"")) %}
-            {% set custom_expr_result_trimmed = custom_expr_result | trim %}
-            {{ log(custom_expr_result_trimmed, info = True) }}
-            {% set renamed_column = "\"" ~  column ~ "\"" ~ " as " ~ "\"" ~  custom_expr_result_trimmed ~ "\"" %}
+        {%- elif renameMethod == 'advancedRename' -%}
+            {%- set custom_expr_result = SnowflakeSqlBasics.evaluate_expression(customExpression | replace('column_name',  "\'" ~ column ~ "\'")) -%}
+            {%- set custom_expr_result_trimmed = custom_expr_result | trim -%}
+            {%- set renamed_column = "\"" ~  column ~ "\"" ~ " AS " ~ "\"" ~  custom_expr_result_trimmed ~ "\"" -%}
+        {%- endif -%}
         
-        {% endif %}
-        
-        {% do renamed_columns.append(renamed_column) %}
-    {% endfor %}
+        {%- do renamed_columns.append(renamed_column) -%}
+    {%- endfor -%}
+
+    {{ log("Renamed column expressions are :" ~ renamed_columns, info=True) }}
+
+    {# Get the schema of cleansed data #}
+    {%- set output_columns = [] -%}
+    {%- for col_name_val in schema -%}
+        {% set flag_dict = {"flag": false} %}
+        {%- for expr in renamed_columns -%}
+            {# Split on 'AS' to get the orig column name; assumes expression contains "AS" #}
+            {%- set parts = expr.split(' AS "') -%}
+            {%- set orig_col_name = parts[0] | trim | replace('"', '') | upper -%}
+            {%- if (col_name_val | trim | replace('"', '') | upper) in orig_col_name -%}
+                {%- do output_columns.append(expr) -%}
+                {% do flag_dict.update({"flag": true}) %}
+                {%- break -%}
+            {%- endif -%}
+        {%- endfor -%}
+
+        {%- if flag_dict.flag == false -%}
+            {%- if "\"" not in col_name_val -%}
+                {%- set col_name = "\"" ~ col_name_val ~ "\"" -%} 
+            {%- else -%}
+                {%- set col_name = col_name_val -%}
+            {%- endif -%}
+            
+            {%- do output_columns.append(col_name) -%}
+        {%- endif -%}
+    {%- endfor -%}        
+    {{ log("Columns after expression evaluation:" ~ output_columns, info=True) }}
+
 
     select 
-        {{ renamed_columns | join(',\n    ') }}
+        {{ output_columns | join(',\n    ') }}
     from {{ relation }}
 {% endmacro %}

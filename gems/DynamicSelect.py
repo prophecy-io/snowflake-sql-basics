40735ee3c95a8728564aa25912c783948e7fab7c
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-04-14 17:38:40 +0530

    Fuzzy match - validation changes

--- gems/DynamicSelect.py
+++ gems/DynamicSelect.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 from prophecy.cb.sql.Component import *
 from prophecy.cb.sql.MacroBuilderBase import *
 from prophecy.cb.ui.uispec import *
-from pyspark.sql import *
 import json
 
 class DynamicSelect(MacroSpec):

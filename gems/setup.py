77ae0a3cb5f298f0f90fd91173bd9d213c9dd8b1
abhinavjain <abhinav@simpledatalabs.com>
2025-06-09 17:28:11 +0530

    dev tag 0.0.5.dev0

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'SnowflakeSqlBasics',
-    version = '0.0.4.dev1',
+    version = '0.0.5.dev0',
     packages = ['SnowflakeSqlBasics'],
     package_dir = {'SnowflakeSqlBasics': '.'},
     description = '',
